7a6e72402bd7cd3958878826549d333eb30e9256
Lorenz <lorenzrenti@outlook.com>
2024-11-09 18:27:58 +0100

    Bugfix: Card overlap fixed

--- lib/widgets/Cards.dart
+++ lib/widgets/Cards.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:fldc/helpers/theme/app_theme.dart';
 import 'package:fldc/helpers/utils/my_shadow.dart';
 import 'package:fldc/helpers/widgets/my_button.dart';
@@ -9,7 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class CardOpenExternal extends StatelessWidget{
+class CardOpenExternal extends StatelessWidget {
   final String title;
   final String description;
   final IconData icon;
@@ -23,81 +24,74 @@ class CardOpenExternal extends StatelessWidget{
     required this.color,
     required this.url,
   });
-  
+
   @override
   Widget build(BuildContext context) {
     return MyCard(
-      height: 130,
       borderRadiusAll: 8,
-      paddingAll: 23,
+      paddingAll: 15,
       shadow: MyShadow(position: MyShadowPosition.bottom, elevation: .5),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: SizedBox(
-                  height: 40,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  description,
+                  style: TextStyle(fontSize: 12),
+                  softWrap: true,
+                ),
+                SizedBox(height: 10),
+                MyButton(
+                  onPressed: () => launchUrl(
+                    Uri.parse(url),
+                    mode: LaunchMode.externalApplication,
+                  ),
+                  elevation: 0,
+                  borderRadiusAll: 8,
+                  padding: MySpacing.xy(20, 16),
+                  backgroundColor: AppTheme.primaryColor,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min, // Minimiert die Breite der Row
                     children: [
-                      Text(
-                        title,
-                        overflow: TextOverflow.ellipsis,
-                        softWrap: true,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                      MyText.labelMedium(
+                        "Open",
+                        fontWeight: 600,
+                        color: Colors.white,
                       ),
-                      Text(
-                        description,
-                        overflow: TextOverflow.ellipsis,
-                        softWrap: true,
-                        style: TextStyle(fontSize: 12),
+                      MySpacing.width(5),
+                      Icon(
+                        LucideIcons.arrow_up_right,
+                        size: 18,
+                        color: Colors.white,
                       ),
                     ],
                   ),
                 ),
-              ),
-              MySpacing.height(5),
-              MyButton(
-                onPressed: () => launchUrl(
-                  Uri.parse(url.toString()),
-                  mode: LaunchMode.externalApplication,
-                ),
-                elevation: 0,
-                borderRadiusAll: 8,
-                padding: MySpacing.xy(20, 16),
-                backgroundColor: AppTheme.primaryColor,
-                child: Row(
-                  children: [
-                    MyText.labelMedium(
-                      "Open",
-                      fontWeight: 600,
-                      color: Colors.white,
-                    ),
-                    MySpacing.width(5),
-                    Icon(
-                      LucideIcons.arrow_up_right,
-                      size: 18,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
+          SizedBox(width: 10),
           MyContainer(
-              paddingAll: 0,
-              height: 54,
-              width: 54,
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              borderRadiusAll: 8,
-              color: color.withAlpha(32),
-              child: Icon(icon, color: color))
+            paddingAll: 0,
+            height: 54,
+            width: 54,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            borderRadiusAll: 8,
+            color: color.withAlpha(32),
+            child: Icon(icon, color: color),
+          ),
         ],
       ),
     );
